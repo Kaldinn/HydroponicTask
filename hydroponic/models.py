15d5a19178777fe2d1972b55c63a96a754a9f436
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-05-29 22:51:24 +0200

    Configure connection with database
    - Add initia models for the application
    - Run and commit initial migrations
    - Secure database connections using django-environ

--- hydroponic/models.py
+++ hydroponic/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class HydroponicSystem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Measurement(models.Model):
+    hydroponic_system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ph = models.FloatField()
+    temperature = models.FloatField()
+    tds = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.hydroponic_system.name} - {self.timestamp}"
