f7488f9d6cccc24b5d84117e7cb5db09fedaa8d7
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-05-31 21:50:11 +0200

    Polish admin pannel
    - Add more configurations to admin pannel,
    to help user interact with it easier

--- hydroponic/admin.py
+++ hydroponic/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import HydroponicSystem, Measurement
-# Register your models here.
 
-admin.site.register(HydroponicSystem)
-admin.site.register(Measurement)
+@admin.register(HydroponicSystem)
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'owner')
+    search_fields = ('name', 'owner__username')
+    list_filter = ('owner',)
+    ordering = ('name',)
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hydroponic_system', 'timestamp', 'ph', 'temperature', 'tds')
+    search_fields = ('hydroponic_system__name', 'timestamp')
+    list_filter = ('hydroponic_system', 'timestamp', 'ph', 'temperature', 'tds')
+    ordering = ('-timestamp',)
+    list_display_links = ('id', 'hydroponic_system')
+    list_per_page = 25
